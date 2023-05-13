68b93fdca892ac3a530a52b6a6d41b28d150e3d6
mario-cartagena <jhonmariocartagena@gmail.com>
2023-05-13 12:45:28 -0500

    testing axios with request get

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
-
+import axios from 'axios';
+const URL = "https://tickets-backend.herokuapp.com/flights";
 const Main = () => {
+  const getFlights = async() => {
+    try {
+      const {data} = await axios.get(URL);
+      console.log(data);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  getFlights();
   return (
     <div>Main</div>
   )
