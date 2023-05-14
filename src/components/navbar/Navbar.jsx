0c9fcd907a0d57294f123cc5ddfd04c80774ee7c
Maria Antonieta Pereira <antonietap0@gmail.com>
2023-05-14 00:05:49 -0500

    add navbar component an styles

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+// import { NavLink } from 'react-router-dom';
+import './styleNavbar.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlaneDeparture } from '@fortawesome/free-solid-svg-icons';
+
+function Navbar() {
+  return (
+    <nav>
+        <div>
+            <span>AIRTICKETS</span>
+            <span><FontAwesomeIcon icon={faPlaneDeparture} style={{color: "#f9fafb",}} /></span>
+        </div>
+       
+      <ul>
+        <li>
+            <button className='btn__link'>
+                LogIn
+            </button>    
+  
+        </li>
+        
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
