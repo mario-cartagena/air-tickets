a35fc95277c82cde96664a786620285989b180a6
Maria Antonieta Pereira <antonietap0@gmail.com>
2023-05-16 22:14:21 -0500

    add data to select airport

--- src/components/search/UseAutocomplete.jsx
+++ src/components/search/UseAutocomplete.jsx
@@ -1,9 +1,11 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import useAutocomplete from '@mui/base/useAutocomplete';
 import { styled } from '@mui/system';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './styleSelectAirport.scss'
+import { getFlights } from '../../services/getAirports';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const Label = styled('label')({
   display: 'block',
@@ -28,6 +30,7 @@ const Listbox = styled('ul')(({ theme }) => ({
   maxHeight: '220px',
   borderRadius: '0px 0px 12px 12px',
   border: '1px solid rgba(0,0,0,.25)',
+  top: '155px',
   '& li.Mui-focused': {
     backgroundColor: '#4a8df6',
     color: 'white',
@@ -52,6 +55,24 @@ const Listbox = styled('ul')(({ theme }) => ({
 }));
 
 export default function UseAutocomplete() {
+  const [flights, setFlights] = useState([]);
+
+  useEffect(() => {
+    getFlights()
+      .then((response) => {
+        if (!flights.length) {
+          setFlights(response);
+        }
+      })
+      .catch((error) => console.log(error));
+  }, [flights]);
+
+  // useEffect(() => {
+  //   flights.forEach((item) => {
+  //     console.log(item.departure_airport.name);
+  //   });
+  // }, [flights]);
+
   const {
     getRootProps,
     getInputLabelProps,
@@ -61,155 +82,40 @@ export default function UseAutocomplete() {
     groupedOptions,
   } = useAutocomplete({
     id: 'use-autocomplete-demo',
-    options: top100Films,
+    options: flights.map((item) => ({
+      title: item.departure_airport.name,
+      code: item.departure_airport.code_iata,
+    })),
     getOptionLabel: (option) => option.title,
-  });
+    isOptionEqualToValue: (option, value) =>
+    option.title === value.title && option.code === value.code, // Personaliza la prueba de igualdad
+    });
 
+    console.log(getInputProps().value)
+
+    const [isDropdownOpen, setDropdownOpen] = useState(true);
+
+    const handleClose = () => {
+      setDropdownOpen(false);
+    };
+   
   return (
     <div className='main__selectAirport'>
       <div {...getRootProps()}>
-        <Label {...getInputLabelProps()}>¿A dónde viajas?</Label>
-        <div  style={{ border:'1px solid', width:'320px', padding:'10px', borderRadius:'12px 12px 0px 0px' }}>
+      <div className='main__selectAirport__label'> <Label {...getInputLabelProps()} className='main__selectAirport__label'>¿A dónde viajas?</Label>  <span onClick={handleClose}><FontAwesomeIcon icon={faXmark} className='car__icon' /></span></div>
+        <div  style={{ width:'320px', padding:'10px', borderRadius:'12px 12px 0px 0px',  border: '1px solid rgba(0,0,0,.25)' }}>
         <FontAwesomeIcon icon={faMagnifyingGlass} style={{ color: 'dark', fontSize: '1rem', marginRight:'.5rem' }}/>
     
-        <Input {...getInputProps()} style={{ border:'0px solid' }} placeholder='Buscar aeropuerto de salida' />
+        <Input {...getInputProps()} style={{ border:'0px solid', outline: 'none' }} placeholder='Buscar aeropuerto de salida' />
         </div>
       </div>
       {groupedOptions.length > 0 ? (
         <Listbox {...getListboxProps()}>
           {groupedOptions.map((option, index) => (
-            <li {...getOptionProps({ option, index })} style={{ display:'flex', justifyContent:'space-between' }}> <div>{option.title}</div> {option.year}</li>
+            <li {...getOptionProps({ option, index })} style={{ display:'flex', justifyContent:'space-between' }}> <div>{option.title}</div> {option.code}</li>
           ))}
         </Listbox>
       ) : null}
     </div>
   );
 }
-
-// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
-const top100Films = [
-  { title: 'The Shawshank Redemption', year: 1994 },
-  { title: 'The Godfather', year: 1972 },
-  { title: 'The Godfather: Part II', year: 1974 },
-  { title: 'The Dark Knight', year: 2008 },
-  { title: '12 Angry Men', year: 1957 },
-  { title: "Schindler's List", year: 1993 },
-  { title: 'Pulp Fiction', year: 1994 },
-  {
-    title: 'The Lord of the Rings: The Return of the King',
-    year: 2003,
-  },
-  { title: 'The Good, the Bad and the Ugly', year: 1966 },
-  { title: 'Fight Club', year: 1999 },
-  {
-    title: 'The Lord of the Rings: The Fellowship of the Ring',
-    year: 2001,
-  },
-  {
-    title: 'Star Wars: Episode V - The Empire Strikes Back',
-    year: 1980,
-  },
-  { title: 'Forrest Gump', year: 1994 },
-  { title: 'Inception', year: 2010 },
-  {
-    title: 'The Lord of the Rings: The Two Towers',
-    year: 2002,
-  },
-  { title: "One Flew Over the Cuckoo's Nest", year: 1975 },
-  { title: 'Goodfellas', year: 1990 },
-  { title: 'The Matrix', year: 1999 },
-  { title: 'Seven Samurai', year: 1954 },
-  {
-    title: 'Star Wars: Episode IV - A New Hope',
-    year: 1977,
-  },
-  { title: 'City of God', year: 2002 },
-  { title: 'Se7en', year: 1995 },
-  { title: 'The Silence of the Lambs', year: 1991 },
-  { title: "It's a Wonderful Life", year: 1946 },
-  { title: 'Life Is Beautiful', year: 1997 },
-  { title: 'The Usual Suspects', year: 1995 },
-  { title: 'Léon: The Professional', year: 1994 },
-  { title: 'Spirited Away', year: 2001 },
-  { title: 'Saving Private Ryan', year: 1998 },
-  { title: 'Once Upon a Time in the West', year: 1968 },
-  { title: 'American History X', year: 1998 },
-  { title: 'Interstellar', year: 2014 },
-  { title: 'Casablanca', year: 1942 },
-  { title: 'City Lights', year: 1931 },
-  { title: 'Psycho', year: 1960 },
-  { title: 'The Green Mile', year: 1999 },
-  { title: 'The Intouchables', year: 2011 },
-  { title: 'Modern Times', year: 1936 },
-  { title: 'Raiders of the Lost Ark', year: 1981 },
-  { title: 'Rear Window', year: 1954 },
-  { title: 'The Pianist', year: 2002 },
-  { title: 'The Departed', year: 2006 },
-  { title: 'Terminator 2: Judgment Day', year: 1991 },
-  { title: 'Back to the Future', year: 1985 },
-  { title: 'Whiplash', year: 2014 },
-  { title: 'Gladiator', year: 2000 },
-  { title: 'Memento', year: 2000 },
-  { title: 'The Prestige', year: 2006 },
-  { title: 'The Lion King', year: 1994 },
-  { title: 'Apocalypse Now', year: 1979 },
-  { title: 'Alien', year: 1979 },
-  { title: 'Sunset Boulevard', year: 1950 },
-  {
-    title: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb',
-    year: 1964,
-  },
-  { title: 'The Great Dictator', year: 1940 },
-  { title: 'Cinema Paradiso', year: 1988 },
-  { title: 'The Lives of Others', year: 2006 },
-  { title: 'Grave of the Fireflies', year: 1988 },
-  { title: 'Paths of Glory', year: 1957 },
-  { title: 'Django Unchained', year: 2012 },
-  { title: 'The Shining', year: 1980 },
-  { title: 'WALL·E', year: 2008 },
-  { title: 'American Beauty', year: 1999 },
-  { title: 'The Dark Knight Rises', year: 2012 },
-  { title: 'Princess Mononoke', year: 1997 },
-  { title: 'Aliens', year: 1986 },
-  { title: 'Oldboy', year: 2003 },
-  { title: 'Once Upon a Time in America', year: 1984 },
-  { title: 'Witness for the Prosecution', year: 1957 },
-  { title: 'Das Boot', year: 1981 },
-  { title: 'Citizen Kane', year: 1941 },
-  { title: 'North by Northwest', year: 1959 },
-  { title: 'Vertigo', year: 1958 },
-  {
-    title: 'Star Wars: Episode VI - Return of the Jedi',
-    year: 1983,
-  },
-  { title: 'Reservoir Dogs', year: 1992 },
-  { title: 'Braveheart', year: 1995 },
-  { title: 'M', year: 1931 },
-  { title: 'Requiem for a Dream', year: 2000 },
-  { title: 'Amélie', year: 2001 },
-  { title: 'A Clockwork Orange', year: 1971 },
-  { title: 'Like Stars on Earth', year: 2007 },
-  { title: 'Taxi Driver', year: 1976 },
-  { title: 'Lawrence of Arabia', year: 1962 },
-  { title: 'Double Indemnity', year: 1944 },
-  {
-    title: 'Eternal Sunshine of the Spotless Mind',
-    year: 2004,
-  },
-  { title: 'Amadeus', year: 1984 },
-  { title: 'To Kill a Mockingbird', year: 1962 },
-  { title: 'Toy Story 3', year: 2010 },
-  { title: 'Logan', year: 2017 },
-  { title: 'Full Metal Jacket', year: 1987 },
-  { title: 'Dangal', year: 2016 },
-  { title: 'The Sting', year: 1973 },
-  { title: '2001: A Space Odyssey', year: 1968 },
-  { title: "Singin' in the Rain", year: 1952 },
-  { title: 'Toy Story', year: 1995 },
-  { title: 'Bicycle Thieves', year: 1948 },
-  { title: 'The Kid', year: 1921 },
-  { title: 'Inglourious Basterds', year: 2009 },
-  { title: 'Snatch', year: 2000 },
-  { title: '3 Idiots', year: 2009 },
-  { title: 'Monty Python and the Holy Grail', year: 1975 },
-];
