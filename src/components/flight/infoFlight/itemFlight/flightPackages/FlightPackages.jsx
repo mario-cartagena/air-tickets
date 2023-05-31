d2dd11b74a87e725ac7558ba56b1d7db3b744bbd
mario-cartagena <jhonmariocartagena@gmail.com>
2023-05-31 01:38:31 -0500

    baggage selected and total

--- src/components/flight/infoFlight/itemFlight/flightPackages/FlightPackages.jsx
+++ src/components/flight/infoFlight/itemFlight/flightPackages/FlightPackages.jsx
@@ -8,9 +8,18 @@ const FlightPackages = ({ id, name, data }) => {
   const { departureInfoBaggage, setDepartureInfoBaggage, arrivalInfoBaggage, setArrivalInfoBaggage } = useContext(AppContext);
   const [baggageSelected, setBaggageSelected] = useState('');
   const [flightConfirmed, setFlightConfirmed] = useState({});
-  const [oneObject,] = useState(30000);
+  const [oneObject] = useState(30000);
   const [handBagagge] = useState(50000);
   const [kgBaggage] = useState(90000);
+
+  console.log(baggageSelected)
+  useEffect(() => {
+    sessionStorage.setItem('departureInfoBaggage', JSON.stringify(departureInfoBaggage))
+  }, [departureInfoBaggage])
+
+  useEffect(() => {
+    sessionStorage.setItem('arrivalInfoBaggage', JSON.stringify(arrivalInfoBaggage))
+  }, [arrivalInfoBaggage])
 
   const handleGetBaggage = (baggage, id, name, data) => {
     setBaggageSelected(baggage);
@@ -25,12 +34,10 @@ const FlightPackages = ({ id, name, data }) => {
     if (flightConfirmed.name === 'departureFlight') {
       // console.log('soy departureFlight')
       setDepartureInfoBaggage(flightConfirmed)
-      sessionStorage.setItem('departureInfoBaggage', JSON.stringify(departureInfoBaggage));
+      
     } else {
       // console.log('soy arrivalFlight')
       setArrivalInfoBaggage(flightConfirmed)
-      sessionStorage.setItem('arrivalInfoBaggage', JSON.stringify(arrivalInfoBaggage));
-
     }
   };
 
@@ -41,21 +48,21 @@ const FlightPackages = ({ id, name, data }) => {
 
   return (
     <div className='baggage'>
-      <div className='baggage__personal' onClick={() => handleGetBaggage(oneObject, id, name, data)}>
+      <div className={`baggage__personal ${baggageSelected === oneObject ? 'active' : ''}`} onClick={() => handleGetBaggage(oneObject, id, name, data)}>
         <figure>
           <img src={BaggageIcon} alt="Icon Package" />
         </figure>
         <span>1 objeto personal</span>
         <span className='baggage__price'>${oneObject} COP</span>
       </div>
-      <div className='baggage__hand' onClick={() => handleGetBaggage(handBagagge, id, name, data)}>
+      <div className={`baggage__hand ${baggageSelected === handBagagge ? 'active' : ''}`} onClick={() => handleGetBaggage(handBagagge, id, name, data)}>
         <figure>
           <img src={BaggageIcon} alt="Icon Package" />
         </figure>
         <span>Equipaje de mano</span>
         <span className='baggage__price'>${handBagagge} COP</span>
       </div>
-      <div className='baggage__25k' onClick={() => handleGetBaggage(kgBaggage, id, name, data)}>
+      <div className={`baggage__25k ${baggageSelected === kgBaggage ? 'active' : ''}`} onClick={() => handleGetBaggage(kgBaggage, id, name, data)}>
         <figure>
           <img src={BaggageIcon} alt="Icon Package" />
         </figure>
